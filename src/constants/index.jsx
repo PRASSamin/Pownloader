c314199de7b1e740327956ce7d77cee58b185b7a
prassamin <prassamin@gmail.com>
2024-12-25 02:06:29 +0600

    v2.0.1 - Stable Release

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,4 +1,5 @@
-export const _tiktokVideoFetchApiByPRAS = 'https://cdn.pras.me/api/tiktok/video?url=';
-export const _tiktokProfileFetchApiByPRAS = 'https://cdn.pras.me/api/tiktok/profile?url=';
-export const _PRASCdnServeitApi = 'https://cdn.pras.me/api/serveit';
-export const _PRASCdnMergeItApi = 'https://cdn.pras.me/api/mergeit';
+export const _cdnHost = 'https://cdn.prassamin.workers.dev';
+export const _tiktokVideoFetchApiByPRAS = `${_cdnHost}/api/tiktok/video?url=`;
+export const _tiktokProfileFetchApiByPRAS = `${_cdnHost}/api/tiktok/profile?url=`;
+export const _PRASCdnMergeItApi = `${_cdnHost}/api/mergeit`;
+export const _PRASCdnServeitApi = `${_cdnHost}/api/serveit`;
